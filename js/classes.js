1cf4fb83763f11e4c1a3f0b0aab47d754ec3ee09
Stepan Tyurin <sanstepon5@yandex.ru>
2022-03-23 17:29:05 +0100

    ajout de la documentation des classes

--- js/classes.js
+++ js/classes.js
@@ -1,4 +1,16 @@
+/**
+ * Classe de l'Anime
+ */
 class Anime{
+    /**
+     * Constructeur de l'Anime
+     *
+     * @param {string} name le nom de l'Anime
+     * @param {string} text la description de l'Anime
+     * @param {string} img l'image de l'Anime (un url ou le nom du fichier)
+     * @param {string} uri l'uri de l'anime
+     * 
+     */
     constructor(name, text, img, uri){
         this.name = name;
         this.text = text;
@@ -7,7 +19,23 @@ class Anime{
     }
 }
 
+/**
+ * Classe de la chanson
+ */
 class Song{
+    /**
+     * Constructeur de la chanson
+     *
+     * @param {string} name le nom de la chanson
+     * @param {string} relation type de la chanson (opening, ending etc)
+     * @param {string} interpreter l'auteur
+     * @param {string} ytb_url le lien youtube (embed)
+     * @param {string} spoty_url le lien spotify (embed)
+     * @param {Anime} anime l'anime auquel correspond la chanson
+     * @param {string} uri l'uri de la chanson
+     * 
+     */
+
     constructor(name, relation, interpreter, ytb_url, spoty_url, anime, uri){
         this.name = name;
         this.relation = relation;
