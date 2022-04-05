a881aceddbb0ffeaef71f6f8284032fc8a82d9ec
Stepan Tyurin <sanstepon5@yandex.ru>
2022-04-05 21:14:41 +0200

    amelioration de la table des chansons (affichage des informations, du cover de l'anime correspondant, des boutons

--- js/functionsSongs.js
+++ js/functionsSongs.js
@@ -6,7 +6,7 @@
     console.log(songs);
     let table = document.getElementById("table");
     table.innerHTML = "";
-    CreateTableHeader(["#", "Titre", "Relation", "Interpreteur", "Liens", "anime"], "song");
+    CreateTableHeader(["#", "Titre", "Relation", "Interpreteur", "Liens", "Anime Cover"], "song");
     
 
     for (const s of songs){
@@ -14,10 +14,7 @@
         //création de la ligne
         let row = CreateSongLine(table, song);
 
-        // Insertion des boutons
-        let { btnEdit, btnSuppr } = CreateButtonsDelEditSong(song);
-        let cellBtn = row.insertCell(4);
-        cellBtn.append(btnEdit, btnSuppr);
+        
     }
 }
 
@@ -28,7 +25,7 @@ const fillSongEditModal = async (self) => {
     let song = await getSong(self.id);
 
     document.getElementById("song-id").innerHTML = song.id;
-    document.querySelector("#songEditionModal #name").value = song.name;
+    document.querySelector("#songEditionModal #name").value = song.title;
     document.querySelector("#songEditionModal #text").value = song.text;
     document.querySelector("#songEditionModal #song-img").setAttribute("src", song.img);
     document.querySelector("#songEditionModal #song-img-url").value = song.img;
@@ -40,8 +37,9 @@ const fillSongEditModal = async (self) => {
  */
  const fillSongDeleteModal = async (self) => {
     let song = await getSong(self.id);
+    console.log(song);
 
-    document.getElementById("song-name2").innerHTML = song.name;
+    document.getElementById("song-name2").innerHTML = song.title;
     document.getElementById("song-id2").innerHTML = self.id;
 
 }
@@ -52,7 +50,7 @@ const fillSongEditModal = async (self) => {
  * @param {Song} song la chanson
  * @returns {HTMLTableRowElement} la ligne à inserer dans le tableau
  */
-function CreateSongLine(table, song) {
+async function CreateSongLine(table, song) {
     let row = table.insertRow(-1);
     row.id = "song-" + song.name;
 
@@ -63,24 +61,59 @@ function CreateSongLine(table, song) {
     let cellName = row.insertCell(1);
     cellName.append(song.name);
 
-    let cellText = row.insertCell(2);
-    cellText.append(song.text);
+    let cellRelation = row.insertCell(2);
+    cellRelation.append(song.relation);
 
-    let cellImg = row.insertCell(3);
-    cellImg.append(song.img);
+    let cellInterpreter = row.insertCell(3);
+    cellInterpreter.append(song.interpreter);
+
+    let cellLiens = row.insertCell(4);
+    insererLiensLigne(song, cellLiens);
+
+    let cellAnime = row.insertCell(5);
+    let anime = await getAnime(song.anime_id);
+    let image = document.createElement("img");
+    if(isValidUrl(anime.img)){
+        image.src = anime.img;
+    }
+    else{
+        image.src = `http://localhost:5000/api/image/${anime.img}`;
+    }
+    image.style.width = "220px";
+    cellAnime.append(image);
+
+    // Insertion des boutons
+    let { btnEdit, btnSuppr } = CreateButtonsDelEditSong(song);
+    btnSuppr.classList.add("ms-2");
+    let cellBtn = row.insertCell(6);
+    let div = document.createElement('div');
+    div.append(btnEdit, btnSuppr)
+    div.classList.add('d-flex');
+    cellBtn.append(div);
+
     return row;
+}
+
+function insererLiensLigne(song, cellLiens) {
+    let lienYoutube = document.createElement("p");
+    lienYoutube.append("https://www.youtube.com/watch?v=" + song.ytb_url);
+    let lienSpotify = document.createElement("p");
+    lienSpotify.append(song.spoty_url);
+    cellLiens.appendChild(lienYoutube);
+    cellLiens.appendChild(lienSpotify);
 }
 
 /**
  * Crée les boutons d'édition et de suppression des chansons
  * @param {Song} song la chanson de la ligne dans le tableau
- * @returns { HTMLButtonElement} {btnEdit, btnSuppr} les deux boutons
+ * @returns { HTMLButtonElement } {btnEdit, btnSuppr} les deux boutons
  */
-function CreateButtonsDelEditSong(song) {
+ const CreateButtonsDelEditSong = (song) => {
     // Insertion du bouton d'édition
     let btnEdit = document.createElement('button');
-    btnEdit.append('Éditer');
+    btnEdit.innerHTML = '<i class="fa fa-bars"></i>';
     btnEdit.type = "button";
+    btnEdit.classList.add("btn", "btn-secondary");
     btnEdit.id = song.id;
     btnEdit.setAttribute('data-bs-toggle', "modal");
     btnEdit.setAttribute('data-bs-target', "#songEditionModal");
@@ -89,7 +122,8 @@ function CreateButtonsDelEditSong(song) {
     };
     // Insertion du bouton de suppression
     let btnSuppr = document.createElement('button');
-    btnSuppr.append('Supprimer');
+    btnSuppr.innerHTML = '<i class="fa fa-trash"></i>';
+    btnSuppr.classList.add("btn", "btn-secondary");
     btnSuppr.id = song.id;
     btnSuppr.setAttribute('data-bs-toggle', "modal");
     btnSuppr.setAttribute('data-bs-target', "#songSupprimerModal");
