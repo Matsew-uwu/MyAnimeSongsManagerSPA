337a1603c5985207a48299a756715174a7cc0f0b
Stepan Tyurin <sanstepon5@yandex.ru>
2022-04-05 21:13:27 +0200

    ajout des fonctions post/put/delete pour les chansons + modif d'un commentaire

--- js/connection.js
+++ js/connection.js
@@ -46,7 +46,7 @@ const getAnime = async (id) => {
 }
 
 /**
- * Envoie les données de l'anime en POST
+ * Envoie les données de l'anime en PUT
  * @param {Anime} anime 
  */
 const putAnime = async (anime) => {
@@ -146,6 +146,136 @@ const SendCreatedAnime = async () => {
     await postAnime(anime);
 }
 
+(async () => {
+    console.log('Lancement scipt : Connexion');
+    console.log(await getAnimes());
+})()
+
+
+
+
+//Chansons
+
+
+/**
+ * Récupère une chanson (GET)
+ * @param {int} id l'id de la chanson à récuperer
+ * @returns {Promise<Song>} promesse avec la chanson qui correspond a l'id
+ */
+ const getSong = async (id) => {
+    return fetch(`http://localhost:5000/api/song/${id}`)
+    .then((response) => {
+        return response.json();
+    })
+    .catch((err) => {
+        console.log(res);
+    })
+    .then((response) => {
+        return response;
+    }); 
+}
+
+/**
+ * Envoie les données de la chanson en PUT
+ * @param {Song} song 
+ */
+const putSong = async (song) => {
+    const uri = `http://localhost:5000/api/song/${song.id}`;
+    fetch(uri, {
+    method: 'PUT',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(song)
+    })
+    .then(res => {
+        return res.json();
+    })
+    .then(res => {
+        console.log(`PUT : ${res}`);
+        refreshTableSongs();
+    })
+    .catch(err => {
+        console.log(err);
+    })
+}
+
+/*const SendEditedSong = async () => {
+    // Récupère les information du formulaire
+    let id = document.getElementById("song-id").innerText;
+    let name = document.getElementById("name").value;
+    let text = document.getElementById("text").value;
+    let img = document.getElementById("anime-img-url").value;
+    let uri = "";
+
+    let anime = new Anime(id, name, text, img, uri);
+    await putAnime(anime);
+}*/
+
+/**
+ * DELETE une chanson
+ * @param {int} id 
+ */
+ const deleteSong = async (id) => {
+    const uri = `http://localhost:5000/api/song/${id}`;
+    fetch(uri, {
+    method: 'DELETE'
+    })
+    .then(res => {
+        return res.json();
+    })
+    .then(res => {
+        console.log(`DELETE : ${res}`);
+        refreshTableSongs();
+    })
+    .catch(err => {
+        console.log(err);
+    })
+}
+
+const SendDeleteSong = async () => {
+    // Récupère les information du formulaire
+    let id = document.getElementById("song-id2").innerText;
+    await deleteSong(id);
+}
+
+/**
+ * Crée une chanson en POST
+ * @param {Song} song 
+ */
+ const postSong = async (song) => {
+    const uri = `http://localhost:5000/api/songs`;
+    fetch(uri, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(song)
+    })
+    .then(res => {
+        return res.json();
+    })
+    .then(res => {
+        console.log(`POST : ${res}`);
+        refreshTableSongs();
+    })
+    .catch(err => {
+        console.log(err);
+    })
+}
+
+/*const SendCreatedSong = async () => {
+    // Récupère les information du formulaire
+    let id = null; //id n'est pas utilisé par l'api
+    let name = document.getElementById("anime-post-name").value;
+    let text = document.getElementById("anime-post-text").value;
+    let img = document.getElementById("anime-post-image").value;
+    let uri = "";
+
+    let anime = new Anime(id, name, text, img, uri);
+    await postAnime(anime);
+}
+*/
 (async () => {
     console.log('Lancement scipt : Connexion');
     console.log(await getAnimes());
